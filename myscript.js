c73701f363dd052ac2970c1f8f4bde1c3162aeae
stfDeveloper <sstefancosmin@icloud.com>
2022-01-17 16:21:29 +0100

    ticket

--- myscript.js
+++ myscript.js
@@ -2,16 +2,36 @@ let app = new Vue({
 
     el: "#app",
     data : {
-        toDos : ["fai i compiti",
-        "chiama quella persona",
-        "fai la spesa",
-        "leggi il libro"]
+        toDos : [
+            {
+                task:"fai i compiti",
+                done: true,
+
+            },
+            {
+                task:"chiama quella persona",
+                done: true,
+            },
+            {
+                task:"fai la spesa",
+                done: true,
+            },
+            {
+                task:"leggi il libro",
+                done: true,
+            },
+    ]
     },
     methods: {
         removeToDo(index) {
           this.toDos.splice(index, 1);
-        }
+        },
+        toggle(){
+            this.done= !done
+            console
+        },
+        
       }
 
-
+      
 })
